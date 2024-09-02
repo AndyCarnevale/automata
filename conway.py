e3f9e8fbbf3b17d571c2d4d186a24c1760c7e75d
AndyCarnevale <469603+AndyCarnevale@users.noreply.github.com>
2024-09-02 09:42:09 -0400

    create Simulation class

--- conway.py
+++ conway.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 import pygame
 
@@ -12,8 +13,16 @@ def create_random_state(x, y, prob=0.5):
     """Create a random initial state of x by y grid"""
     return np.random.choice([False, True], size=(x, y), p=[1-prob, prob])
 
-height, width = 1000, 1000
-X = create_random_state(height, width, prob=0.2)
+class Simulation:
+    def __init__(self, size: Tuple[int, int]) -> None:
+        self.size = size
+        self.state = create_random_state(*size)
+        
+    def update(self) -> None:
+        self.state = conway_step(self.state)
+
+width, height = 1000, 1000
+conway = Simulation((width, height))
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -25,12 +34,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    X = conway_step(X)
-    
-    pygame.surfarray.blit_array(surface, X * 0xFFFFFF)
+    conway.update()
+
+    pygame.surfarray.blit_array(surface, conway.state * 0xFFFFFF)
     screen.blit(surface, (0, 0))
-    
+
     pygame.display.flip()
     clock.tick(10)  # Limit to 10 frames per second
 
