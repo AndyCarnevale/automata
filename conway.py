9aafdb6426c8ee891ba6c271bcfb28552d3a666a
AndyCarnevale <469603+AndyCarnevale@users.noreply.github.com>
2024-09-02 12:34:25 -0400

    create abstract classes for simulation rules

--- conway.py
+++ conway.py
@@ -1,25 +1,44 @@
-from typing import Tuple
+from typing import Any, Tuple
+from abc import ABC, abstractmethod
 import numpy as np
 import pygame
 
-def conway_step(X: np.ndarray) -> np.ndarray:
-    """Conway's Game of Life step using numpy array X"""
-    neighbors_count = sum(np.roll(np.roll(X, i, 0), j, 1)
-                    for i in (-1, 0, 1) for j in (-1, 0, 1)
-                    if (i != 0 or j != 0))
-    return (neighbors_count == 3) | (X & (neighbors_count == 2))
+class InitialStateRule(ABC):
+    @abstractmethod
+    def rule(self, size: Tuple[int, int]) -> np.ndarray:
+        pass
 
-def create_random_state(size: Tuple[int, int], prob: float = 0.2) -> np.ndarray:
-    """Create a random initial state of x by y grid"""
-    return np.random.choice([False, True], size=size, p=[1-prob, prob])
+class UpdateRule(ABC):
+    @abstractmethod
+    def update(self, current_state: np.ndarray) -> np.ndarray:
+        pass
+
+class RandomInit(InitialStateRule):
+    def __init__(self, p: float = 0.5) -> None:
+        self.p = p
+    def rule(self, size) -> np.ndarray:
+        return np.random.choice([0, 1], size=size, p=[1-self.p, self.p])
+
+class ConwayUpdate(UpdateRule):
+    def update(self, current_state: np.ndarray) -> np.ndarray:
+        neighbors_count = sum(np.roll(np.roll(current_state, i, 0), j, 1)
+                for i in (-1, 0, 1) for j in (-1, 0, 1)
+                if (i != 0 or j != 0))
+        return (neighbors_count == 3) | (current_state & (neighbors_count == 2))
 
 class SimulationModel:
-    def __init__(self, size: Tuple[int, int]) -> None:
+    def __init__(self,
+                 size: Tuple[int, int],
+                 init_rule: InitialStateRule,
+                 sim_rule: UpdateRule
+                 ) -> None:
         self.size = size
-        self.state = create_random_state(size)
+        self.init_rule = init_rule
+        self.sim_rule = sim_rule
+        self.state = init_rule.rule(size)
 
     def update(self) -> None:
-        self.state = conway_step(self.state)
+        self.state = self.sim_rule.update(self.state)
 
 class SimulationView:
     def __init__(self, window_size: Tuple[int, int]) -> None:
@@ -32,10 +51,9 @@ class SimulationView:
         pygame.display.flip()
 
 class SimulationController:
-    def __init__(self, size: Tuple[int, int]) -> None:
-        self.size = size
-        self.model = SimulationModel(size)
-        self.view = SimulationView(size)
+    def __init__(self, model: SimulationModel, view: SimulationView) -> None:
+        self.model = model
+        self.view = view
 
         pygame.init()
         self.clock: pygame.time.Clock = pygame.time.Clock()
@@ -56,7 +74,17 @@ class SimulationController:
 def main():
     width: int = 1000
     height: int = 1000
-    controller = SimulationController((width, height))
+    grid_size = (width, height)
+    window_size = (width, height)
+
+    init_rule = RandomInit(p=0.75)
+    sim_rule = ConwayUpdate()
+    model = SimulationModel(grid_size, init_rule, sim_rule)
+
+    view = SimulationView(window_size)
+
+    controller = SimulationController(model, view)
+
     running = True
     while running:
         controller.handle_events()
